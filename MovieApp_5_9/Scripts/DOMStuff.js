82cacfa67cb97674b1ab99e0b1a8fff671e621a3
Jaclyn Noga <jaclyn.noga@gmail.com>
2016-06-09 15:24:58 -0400

    setting up repo

--- MovieApp_5_9/Scripts/DOMStuff.js
+++ MovieApp_5_9/Scripts/DOMStuff.js
@@ -5,4 +5,5 @@
 function changeLikes() {
     var likesParagraph = document.getElementById('likes');
     likesParagraph.innerHTML = "I like dogs and cats"
+
 }
---
+++ MovieApp_5_9/Scripts/QuoteGenerator.js
@@ -0,0 +1,13 @@
+﻿function CycleQuotes() {
+    var quotes = [
+        "These aren't the droids you're looking for",
+        "I am your father",
+        "We're gonna need a bigger boat",
+        "Houston we have a problem",
+        "Don't get homeboy fired up",
+    ];
+
+    var quoteElement = document.getElementById('quote');
+
+    Math.random()
+}
